d42263a8af0bd77fbf5637343fb324e2d1b31df9
Jason Pierna <siilver777@me.com>
2018-07-05 15:18:50 +0200

    Crypt/decrypt done

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func padding(text string, blockSize int) []string {
 
 		end := i + blockSize
 		if end > len(text) - 1 {
-			end = len(text) - 1
+			end = len(text)
 		}
 
 		slice := text[i:end]
@@ -48,20 +48,27 @@ func padding(text string, blockSize int) []string {
 	return array
 }
 
-func cipher(text string, direction int) string {
-	shift, offset := rune(3), rune(26)
+func abs(number int) (int) {
+	if number < 0 {
+		return -number
+	}
+	return number
+}
+
+func cesar(text string, shiftNumber int) string {
+	shift, offset := rune(abs(shiftNumber) % 26), rune(26)
 
 	runes := []rune(text)
 
 	for index, char := range runes {
-		switch direction {
-		case -1:
+
+		if shiftNumber < 0 {
 			if char >= 'a'+shift && char <= 'z' || char >= 'A'+shift && char <= 'Z' {
 				char = char - shift
 			} else if char >= 'a' && char < 'a'+shift || char >= 'A' && char < 'A'+shift {
 				char = char - shift + offset
 			}
-		case +1:
+		} else {
 			if char >= 'a' && char <= 'z'-shift || char >= 'A' && char <= 'Z'-shift {
 				char = char + shift
 			} else if char > 'z'-shift && char <= 'z' || char > 'Z'-shift && char <= 'Z' {
@@ -75,23 +82,99 @@ func cipher(text string, direction int) string {
 	return string(runes)
 }
 
+func xor(first string, second string) (string) {
+	var length int
 
+	if len(first) > len(second) {
+		length = len(second)
+	} else {
+		length = len(first)
+	}
+
+	bytes := make([]byte, length)
+
+	for i := 0; i < length; i++ {
+		bytes[i] = first[i] ^ second[i]
+	}
+
+	return string(bytes)
+}
+
+func unxor(first string, second string) (string) {
+	var length int
+
+	if len(first) > len(second) {
+		length = len(second)
+	} else {
+		length = len(first)
+	}
+
+	bytes := make([]byte, length)
+
+	for i := 0; i < length; i++ {
+		bytes[i] = (^first[i] & second[i]) | (first[i] & ^second[i])
+	}
+
+	return string(bytes)
+
+	/*
+&   bitwise AND
+|   bitwise OR
+^   bitwise XOR
+&^   AND NOT
+<<   left shift
+>>   right shift
+ */
+}
 
 func main() {
-	//key := "1234"
-	text := "Bonjour encule de ta race il n y a pas assez de caracteres donc j en rajoute car tu es un sale connard espece de Kevin de merde est-ce que t entends ca, ca depasse l'entendement fils de pute."
+	key := "abcdefghijklmnop"
+	text := "Bonjour encule de ta race il n y a pas assez de caracteres donc j en rajoute car tu es un sale connard espece de Kevin de merde est ce que t entends ca, ca depasse l entendement fils de pute onche onche onche test de Jason qui pue des fesses et qui mange des zizis de maeva et en plus il est en couple avec elle xoxop"
 
-	/*padding := padding(text, 4)
+	println("Taille texte de base", len(text))
 
-	for index, txt := range padding {
+	// Découpe en blocs
+	paddingArray := padding(text, 16)
+
+	// Création du tableau final
+	encrypted := make([]string, 0)
+
+	for index, txt := range paddingArray {
 		println(index, txt)
-	}*/
 
-	encoded := cipher(text, -1)
-	decoded := cipher(encoded, +1)
+		cryptedKey := cesar(key, index)
+		encrypted = append(encrypted, xor(txt, cryptedKey))
+	}
 
-	println(encoded)
-	println(decoded)
+	finalString := ""
+
+	for index, txt := range encrypted {
+		println(index, txt)
+		finalString += txt
+	}
+
+	println(finalString)
+	println("Taille texte encrypté", len(finalString))
+
+	////////// Déchiffrement
+
+	decryptPadding := padding(finalString, 16)
+	decrypted := make([]string, 0)
+
+	for index, txt := range decryptPadding {
+
+		cryptedKey := cesar(key, index)
+		decrypted = append(decrypted, unxor(cryptedKey, txt))
+	}
+
+	decryptedString := ""
+	for index, txt := range decrypted {
+		println(index, txt)
+		decryptedString += txt
+	}
+
+	println(decryptedString)
+	println("Taille texte décrypté", len(decryptedString))
 }
 
 func listFiles(path string) ([]string) {
